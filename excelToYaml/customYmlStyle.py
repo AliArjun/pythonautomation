b1e6ef690bfb1ed30469e6c9bd665b74de9f9b9f
Ali Arjun <ndamazon20@gmail.com>
2023-09-15 14:01:12 -0400

    fix yaml format

--- excelToYaml/customYmlStyle.py
+++ excelToYaml/customYmlStyle.py
@@ -6,15 +6,18 @@ data = {
     "city": "New York",
 }
 
-# Create a custom style function to apply double quotes to specific attributes
-def custom_style(dumper, tag, value):
-    if tag == "!!str":
+# Create a custom representer function to apply double quotes to specific attributes
+def custom_representer(dumper, data):
+    if isinstance(data, str) and dumper.alias_key is None:
         attribute_name = dumper.serializer.processed[-1]
         if attribute_name == "city":
-            return dumper.represent_scalar(tag, value, style='"')
-    return dumper.represent_scalar(tag, value)
+            return dumper.represent_scalar("tag:yaml.org,2002:str", data, style='"')
+    return dumper.represent_scalar("tag:yaml.org,2002:str", data)
 
-# Use the custom style function when dumping the data to YAML
-yaml_string = yaml.dump(data, default_style=custom_style)
+# Register the custom representer function
+yaml.add_representer(str, custom_representer)
+
+# Use yaml.dump() to serialize the data
+yaml_string = yaml.dump(data)
 
 print(yaml_string)
